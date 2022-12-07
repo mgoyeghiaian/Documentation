eddb99b34bc83d09554f2e30ec526799a17ac60f
mgoyeghiaian <mgoyeghiaian@gmail.com>
2022-12-07 15:04:06 +0200

    Js_Basics1 Step5

---
+++ 03_Exercises/03_JavaScript/javascript-basics-01/Step_5.js
@@ -0,0 +1,5 @@
+function showalert(){
+  var First_Number = document.getElementById("first_number").value;
+  var Second_Number = document.getElementById("second_number").value;
+  alert(First_Number*Second_Number);
+ }
