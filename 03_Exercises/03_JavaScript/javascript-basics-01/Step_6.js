829cab14ded5c77e31574349d652c2337861f14d
mgoyeghiaian <mgoyeghiaian@gmail.com>
2022-12-07 16:11:27 +0200

    Js_Basics1 Step6

--- 03_Exercises/03_JavaScript/javascript-basics-01/Step_6.js
+++ 03_Exercises/03_JavaScript/javascript-basics-01/Step_6.js
@@ -1,4 +1,4 @@
-function divide(){
+function showalert(){
   var First_Number = document.getElementById("first_number").value;
   var Second_Number = document.getElementById("second_number").value;
   alert(First_Number/Second_Number);
