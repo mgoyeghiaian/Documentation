d10f5fe653f5b6f22ca35919a9caa760513dedd7
mgoyeghiaian <mgoyeghiaian@gmail.com>
2022-12-07 13:34:09 +0200

    Js_Basics1 Step4

---
+++ 03_Exercises/03_JavaScript/javascript-basics-01/Step_4.js
@@ -0,0 +1,6 @@
+ function showalert(){
+  var Name = document.getElementById("name").value;
+  var Surname = document.getElementById("surname").value;
+  var City = document.getElementById("city").value;
+  alert (Name +"\n"+ Surname +"\n"+ City);
+ }
