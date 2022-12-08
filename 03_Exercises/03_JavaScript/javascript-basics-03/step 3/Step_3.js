31352ab9bce57959504a14103eddd4af57744a75
mgoyeghiaian <mgoyeghiaian@gmail.com>
2022-12-08 13:21:31 +0200

    Js_Basics03 Step3

---
+++ 03_Exercises/03_JavaScript/javascript-basics-03/step 3/Step_3.js
@@ -0,0 +1,11 @@
+function colorG() {
+  document.getElementById("text").style.color = "green";
+}
+
+function colorR() {
+  document.getElementById("text").style.color = "red";
+}
+
+function colorB() {
+  document.getElementById("text").style.color = "blue";
+}
