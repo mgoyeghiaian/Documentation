80cdb89fb34cc1158796627d3745e638ba037a1c
mgoyeghiaian <mgoyeghiaian@gmail.com>
2022-12-08 15:16:19 +0200

    Revert "Revert "Js_Basics03 Step2""
    This reverts commit 37f89d93a2fcfea84cb3c12e899681ede34534d1.

---
+++ 03_Exercises/03_JavaScript/javascript-basics-03/step 2/Step_2.js
@@ -0,0 +1,9 @@
+function button(use) {
+  var texte = document.getElementById("texte");
+  if (use === "show") {
+    texte.style.display = "inline";
+  } else if (use === "hide") {
+    texte.style.display = "none";
+  }
+
+}
