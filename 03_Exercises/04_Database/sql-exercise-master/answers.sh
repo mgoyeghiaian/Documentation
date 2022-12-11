10dd5d3524d362624695449f659044a077f2430f
mgoyeghiaian <mgoyeghiaian@gmail.com>
2022-12-11 09:37:05 +0200

    Creating Table

--- 03_Exercises/04_Database/sql-exercise-master/answers.sh
+++ 03_Exercises/04_Database/sql-exercise-master/answers.sh
@@ -1,3 +1,4 @@
+*************Basic Queries****************************************
 #1. GET ALL THE NAMES OF STUDENTS IN THE DATABASE
 SELECT * FROM `students`
 
@@ -19,3 +20,31 @@ UPDATE students SET Points = Points +200 WHERE name="Basma"
 
 #7. DECREASE THE POINTS OF ALEX BECAUSE HE'S LATE TODAY 8
 UPDATE students SET Points = Points -8 WHERE name="Alex"
+**************************************************************************
+
+******************Creating Table********************************************
+# 1. Create a table graduates
+CREATE TABLE "graduates" (
+	"ID"	INTEGER NOT NULL,
+	"Name"	TEXT NOT NULL UNIQUE,
+	"Age"	INTEGER,
+	"Gender"	TEXT,
+	"Points"	INTEGER,
+	"Graduation"	TEXT,
+	PRIMARY KEY("ID" AUTOINCREMENT)
+);
+
+#2.Copy Layal's data from students to graduates
+
+INSERT INTO graduates(ID,name,Age,Gender,Points)
+SELECT * FROM students WHERE ID = 4
+
+#3.Add the graduation date previously mentioned to Layal's record in graduates
+UPDATE graduates
+SET Graduation = '08/09/2018'
+WHERE ID = 4;
+
+
+#5. Remove Layal's record from students
+DELETE FROM students WHERE ID = 4;
+
