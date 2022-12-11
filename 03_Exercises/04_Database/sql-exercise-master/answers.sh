7771cf9c4e8f0d67da6b2739552b62108b1934ad
mgoyeghiaian <mgoyeghiaian@gmail.com>
2022-12-11 13:08:23 +0200

    Joins

--- 03_Exercises/04_Database/sql-exercise-master/answers.sh
+++ 03_Exercises/04_Database/sql-exercise-master/answers.sh
@@ -45,6 +45,20 @@ SET Graduation = '08/09/2018'
 WHERE ID = 4;
 
 
-#5. Remove Layal's record from students
+#4. Remove Layal's record from students
 DELETE FROM students WHERE ID = 4;
+*******************************************************************
+
+******************Joins********************************************
+
+#1. Produce a table that contains, for each employee, his/her name, company name, and company date.
+SELECT employees.Name, employees.Company, companies.Date FROM employees,companies WHERE employees.Company = companies.Name;
+
+#2.Find the name of employees that work in companies made before 2000.
+SELECT employees.Name FROM companies, employees WHERE employees.Company = companies.Name AND companies.date < 2000;
+
+#3.Find the name of company that has a graphic designer.
+SELECT companies.Name FROM employees, companies WHERE employees.Company = companies.Name AND employees.Role='Graphic Designer';
+
+***********************************************************************
 
