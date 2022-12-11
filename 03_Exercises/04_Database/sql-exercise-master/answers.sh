5ddd796417a12b6003616335116256822e9148cd
mgoyeghiaian <mgoyeghiaian@gmail.com>
2022-12-11 13:32:42 +0200

    Creating Table

--- 03_Exercises/04_Database/sql-exercise-master/answers.sh
+++ 03_Exercises/04_Database/sql-exercise-master/answers.sh
@@ -1,25 +1,30 @@
-*************Basic Queries****************************************
-#1. GET ALL THE NAMES OF STUDENTS IN THE DATABASE
-SELECT * FROM `students`
 
-#2. GET ALL THE DATA OF STUDENTS ABOVE 30 YEARS OLD
-SELECT * FROM `students` WHERE age>30
 
-#3. GET THE NAMES OF THE FEMALES WHO ARE 30 YEARS OLD
-SELECT * FROM students WHERE Gender="F" AND Age = 30
+******************Creating Table********************************************
+# 1. Create a table graduates
+CREATE TABLE "graduates" (
+	"ID"	INTEGER NOT NULL,
+	"Name"	TEXT NOT NULL UNIQUE,
+	"Age"	INTEGER,
+	"Gender"	TEXT,
+	"Points"	INTEGER,
+	"Graduation"	TEXT,
+	PRIMARY KEY("ID" AUTOINCREMENT)
+);
 
-#4. GET THE NUMBER OF POINTS OF ALEX
-SELECT Points FROM students WHERE name="Alex"
+#2.Copy Layal's data from students to graduates
 
-#5. ADD YOURSELF AS A NEW STUDENT (YOUR NAME, YOUR AGE...)
-INSERT INTO students (Name,Age,Gender,Points)
-VALUES ("Mgo","25","M","600")
+INSERT INTO graduates(ID,name,Age,Gender,Points)
+SELECT * FROM students WHERE ID = 4
 
-#6. INCREASE THE POINTS OF BASMA BECAUSE SHE SOLVED A NEW EXERCISE 
-UPDATE students SET Points = Points +200 WHERE name="Basma"
+#3.Add the graduation date previously mentioned to Layal's record in graduates
+UPDATE graduates
+SET Graduation = '08/09/2018'
+WHERE ID = 4;
 
-#7. DECREASE THE POINTS OF ALEX BECAUSE HE'S LATE TODAY 8
-UPDATE students SET Points = Points -8 WHERE name="Alex"
-**************************************************************************
+
+#4. Remove Layal's record from students
+DELETE FROM students WHERE ID = 4;
+*******************************************************************
 
 
